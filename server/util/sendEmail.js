9deac27b0475b93b4b0a1e7a2d55739e7fa3e4db
Anshika Gupta <rimigupta123456@gmail.com>
2026-06-26 12:10:43 +0530

    fix sendEmail

--- server/util/sendEmail.js
+++ server/util/sendEmail.js
@@ -1,21 +1,23 @@
 const nodemailer = require("nodemailer");
 
 const sendMail = async ({ to, subject, text }) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+  const response = await fetch("https://resend.com/docs/create-an-api-key", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.RESEND_API}`,
+      "Content-type": "application/json",
     },
+    body: JSON.stringify({
+      from: "onboarding@resend.dev",
+      to,
+      subject,
+      test,
+    }),
   });
-
-  const info = await transporter.sendMail({
-    from: `"Auth App" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    text,
-  });
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || "Failed to send email");
+  }
 };
 
 module.exports = sendMail;
